18cf7f381263319e75eaa3832aed65267330583a
이해규 <gorb6593@naver.com>
2023-05-22 01:00:57 +0900

    live reload setting

---
+++ sbb/src/main/java/com/ll/exam/sbb/MainController.java
@@ -0,0 +1,18 @@
+package com.ll.exam.sbb;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    // 아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
+    @ResponseBody
+    public String index() {
+        return "안녕하세요.";
+    }
+}
