9bfe25406096d868c4206063b14221c9c458c712
gorb6593 <gorb6593@gmail.com>
2023-05-22 01:51:10 +0900

    day 1

--- sbb/src/main/java/com/ll/exam/sbb/MainController.java
+++ sbb/src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,8 +2,7 @@ package com.ll.exam.sbb;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -14,5 +13,32 @@ public class MainController {
     @ResponseBody
     public String index() {
         return "hello world!";
+    }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showGet() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이입력" />
+                    <input type="submit" value="page2로 POST방식으로 이동" />
+                </form>
+                """;
+    }
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 :  %d</h1>
+                <h1>안녕하세요. POST로 왔음</h1>
+                """.formatted(age);
+    }
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 :  %d</h1>
+                <h1>안녕하세요. POST로 왔음</h1>
+                """.formatted(age);
     }
 }
