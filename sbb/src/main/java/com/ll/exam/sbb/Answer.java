a899cd8a6d5496f2558a3ab789f8e82ca86297fe
gorb6593 <gorb6593@gmail.com>
2023-06-04 01:40:33 +0900

    db연동 후 crud

---
+++ sbb/src/main/java/com/ll/exam/sbb/Answer.java
@@ -0,0 +1,30 @@
+package com.ll.exam.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private Question question;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+}
